63704a10f86d4e659daedc0c5356e48215aff919
MD. AHANAF MOHOSEN <ahanaf537@gmail.com>
2024-11-23 03:43:41 +0600

    8th published

--- Chapter 9 - Misc/9_15.c
+++ Chapter 9 - Misc/9_15.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 typedef int *ip;
 
-int main(int argc char *argv[])
+int main(int argc, char *argv[])
 {
   int *a;  // a is an integer pointer
   ip b;    // b is also an integer pointer
